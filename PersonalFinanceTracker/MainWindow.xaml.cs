6d1a38ca09f66284fa9b1dbc7ed6117a57e69908
Mille Elfver <mille.elfver98@gmail.com>
2024-06-09 14:58:04 +0200

    Solved issue with login, now username no longer null

--- PersonalFinanceTracker/MainWindow.xaml.cs
+++ PersonalFinanceTracker/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PersonalFinanceTracker.ViewModels;
 
 namespace PersonalFinanceTracker;
 
@@ -19,5 +20,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        this.DataContext = new MainViewModel();
     }
 }
