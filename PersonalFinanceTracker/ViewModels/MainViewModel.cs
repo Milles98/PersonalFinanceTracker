ff7070643aa58db7f0ac925da5ddb37cb0185e6b
Mille Elfver <mille.elfver98@gmail.com>
2024-06-09 13:18:51 +0200

    Added initial design of frontpage and updated MainViewModel.cs

--- PersonalFinanceTracker/ViewModels/MainViewModel.cs
+++ PersonalFinanceTracker/ViewModels/MainViewModel.cs
@@ -7,7 +7,6 @@ namespace PersonalFinanceTracker.ViewModels;
 public class MainViewModel : INotifyPropertyChanged
 {
     private ObservableCollection<Transaction> _transactions = null!;
-
     public ObservableCollection<Transaction> Transactions
     {
         get => _transactions;
@@ -16,6 +15,15 @@ public class MainViewModel : INotifyPropertyChanged
             _transactions = value;
             OnPropertyChanged(nameof(Transactions));
         }
+    }
+    
+    public MainViewModel()
+    {
+        Transactions =
+        [
+            new Transaction { Description = "Groceries", Amount = 50, Category = "Food", Date = DateTime.Now },
+            new Transaction { Description = "Rent", Amount = 1200, Category = "Housing", Date = DateTime.Now }
+        ];
     }
     
     public event PropertyChangedEventHandler? PropertyChanged;
