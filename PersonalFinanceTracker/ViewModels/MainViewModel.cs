cf965f05d9803f88aa2dffdc359575a1db9b68b0
Mille Elfver <mille.elfver98@gmail.com>
2024-06-09 13:48:25 +0200

    Added relaycommand to handle button commands, updated MainViewModel.cs

--- PersonalFinanceTracker/ViewModels/MainViewModel.cs
+++ PersonalFinanceTracker/ViewModels/MainViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
+using PersonalFinanceTracker.Command;
 using PersonalFinanceTracker.Models;
+using PersonalFinanceTracker.Views;
 
 namespace PersonalFinanceTracker.ViewModels;
 
@@ -16,6 +19,21 @@ public class MainViewModel : INotifyPropertyChanged
             OnPropertyChanged(nameof(Transactions));
         }
     }
+
+    private object _currentView;
+    public object CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged(nameof(CurrentView));
+        }
+    }
+    
+    public ICommand ShowLoginViewCommand { get; }
+    public ICommand ShowTransactionEntryViewCommand { get; }
+    public ICommand ShowTransactionHistoryViewCommand { get; }
     
     public MainViewModel()
     {
@@ -24,10 +42,17 @@ public class MainViewModel : INotifyPropertyChanged
             new Transaction { Description = "Groceries", Amount = 50, Category = "Food", Date = DateTime.Now },
             new Transaction { Description = "Rent", Amount = 1200, Category = "Housing", Date = DateTime.Now }
         ];
+
+        ShowLoginViewCommand = new RelayCommand(ShowLoginView);
+        ShowTransactionEntryViewCommand = new RelayCommand(ShowTransactionEntryView);
+        ShowTransactionHistoryViewCommand = new RelayCommand(ShowTransactionHistoryView);
     }
+
+    private void ShowLoginView(object obj) => CurrentView = new LoginView();
+    private void ShowTransactionEntryView(object obj) => CurrentView = new TransactionEntryView();
+    private void ShowTransactionHistoryView(object obj) => CurrentView = new TransactionHistoryView();
     
     public event PropertyChangedEventHandler? PropertyChanged;
-
     private void OnPropertyChanged(string propertyName)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
